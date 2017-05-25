8f72eda1369008987fdcad4bb693faf290887b45
John Wiggins <john.wiggins@xfel.eu>
2017-05-25 11:59:27 +0200

    Bump version to 0.0.4

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ elif sys_os == 'darwin':
     requirements.append('machotools')
 
 setup(name='rpathology',
-      version='0.0.3',
+      version='0.0.4',
       license='MIT',
       author='John Wiggins',
       author_email='john.wiggins@xfel.eu',
