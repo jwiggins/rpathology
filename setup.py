1a3653c76bface50d1f14ae35ee4c57d2e1bbcc7
John Wiggins <john.wiggins@xfel.eu>
2017-05-25 11:58:26 +0200

    Version 0.0.3. Now with packaging metadata!

--- setup.py
+++ setup.py
@@ -11,11 +11,26 @@ elif sys_os == 'darwin':
     requirements.append('machotools')
 
 setup(name='rpathology',
-      version='0.0.2',
+      version='0.0.3',
+      license='MIT',
       author='John Wiggins',
       author_email='john.wiggins@xfel.eu',
       description='A library for dealing with executable RPATHs',
       long_description='',
+      url='https://github.com/jwiggins/rpathology',
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Environment :: Console',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 3',
+          'Topic :: Software Development :: Libraries',
+          'Operating System :: POSIX',
+          'Operating System :: Unix',
+          'Operating System :: MacOS',
+      ],
       packages=find_packages(),
       package_data={},
       entry_points={
